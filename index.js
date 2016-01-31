92456d1d3093c5b4cd17ad62eaa21683ebc6315f
Andrea Parodi <andrea@parro.it>
2016-01-31 10:17:43 +0100

    first release

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 'use strict';
 
-module.exports = function runscripts() {
-  return 42;
-};
+const pkgConf = require('pkg-conf');
+const co = require('co');
+const spawn = require('spawn-shell');
+
+function error(code, message) {
+  const err = new Error(message);
+  err.code = code;
+  throw err;
+}
+
+function * runScripts(scriptName, options) {
+  const scripts = yield pkgConf('scripts', options.cwd);
+  if (! scripts.hasOwnProperty(scriptName)) {
+    error('ENOSCRIPT', `Script not found: ${scriptName}\n`);
+  }
+  const script = scripts[scriptName];
+  return spawn(script, options.spawn);
+}
+
+module.exports = co.wrap(runScripts);
--- test.js
+++ test.js
@@ -1,8 +1,31 @@
 const test = require('tape');
-const runscripts = require('./');
+const run = require('./');
+const co = require('co');
 
-test('it work!', t => {
-  const result = runscripts();
-  t.equal(result, 42);
-  t.end();
+const concat = require('stream-string');
+
+const check = co.wrap(function * check(dir, script) {
+  const result = yield run(script, {
+    cwd: __dirname + `/fixtures/${dir}`,
+    spawn: {
+      stdio: [0, 'pipe', 2]
+    }
+  });
+
+  return concat(result.stdout);
 });
+
+test('run scripts from pkg.json', t => co(function * () {
+  t.equal(yield check('simple', 'test1'), 'ciao\n');
+  t.end();
+}).catch(err => t.end(err)));
+
+
+test('throws when script not found', t => co(function * () {
+  check('simple', 'non-existant')
+    .catch(err => {
+      t.equal(err.code, 'ENOSCRIPT');
+      t.end();
+    });
+
+}));
